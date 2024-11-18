6d291a8a19ec350fe85f66e0323b91a08ef2b1fd
root <root@centralautomation.pedrorocha.cc>
2024-11-18 20:48:16 +0000

    update

---
+++ motor/packer/task_templating.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+START_PATH=$(pwd)
+create_templates(){
+    for directory in $(ls -d */); do
+        cd $directory
+        packer init .
+        echo "[+] building template in: $(pwd)"
+        packer build .
+        cd ..
+    done;
+}
+create_templates
+echo "[+] run the task_terraforming.sh in terraform/"
